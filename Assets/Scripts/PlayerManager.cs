06b707acb7ac97b39c814a8eb6b8f7ff07e05340
Emily <emidude@gmail.com>
2022-08-24 16:51:32 +0100

    put a short audio clip in for first song to play for debugging, client keeps getting disconnected at some point

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -75,7 +75,7 @@ public class PlayerManager : NetworkBehaviour
         base.OnStartLocalPlayer();
 
         //SET ORDERING:
-        songOrdering = new int[] { 1, 2, 2, 3, 6, 9};
+        songOrdering = new int[] { 4, 2, 2, 3, 6, 9};
         combinations = new int[] { 4, 2, 1, 3, 0, 0};
 
         //AUDIO:
