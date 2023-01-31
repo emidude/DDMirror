846e9f4073c1d31b8c7d36d76a78d2ddf58a22c1
Emily <emidude@gmail.com>
2023-01-31 16:13:19 +0000

    fixed bug in rotations

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -425,7 +425,7 @@ public class PlayerManager : NetworkBehaviour
         float[] rotationLerpParam = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1 };
         for (int i = 0; i < 16; i++)
         {
-            rotationLerpParam[i] = positionScaleFactors[i] / 8;
+            rotationLerpParam[i] = rotationLerpParam[i] / 8;
         }
 
 
