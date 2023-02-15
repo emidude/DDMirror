63bda8d1c361ce648b3f8d64b3b1fb82a5bef766
Emily <emidude@gmail.com>
2023-02-15 18:15:35 +0000

    ga

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -103,8 +103,8 @@ public class PlayerManager : NetworkBehaviour
         ContinuousLogger = audioObject.GetComponent<ContinuousLogger>();
 
         //ORDERING:
-        sessionOrdering = new int[] {3,1,2,0 };
-        songOrdering = new int[] {3,4,6,7};
+        sessionOrdering = new int[] {1,3,0,2 };
+        songOrdering = new int[] {6,0,1,5};
 
         //PANELS/////////////////////MISTAEK BELOW IDK?
         guiObject = GameObject.FindGameObjectWithTag("PanelParent");
