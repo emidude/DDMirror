887cb68917624149fcc1faa28b890499a3fb6ebb
Emily <emidude@gmail.com>
2023-05-08 19:13:42 +0100

    got little cubes moving, too fast atm, renamed vars headGO  to P1headGO etc for showing both dancers at the same time, I'm pretty sure this has fucked with the other scripts as well though so good that this playback is on a separate branch becasue the study branch likely does not work on this one now

--- Assets/Scripts/CsvReader.cs
+++ Assets/Scripts/CsvReader.cs
@@ -7,42 +7,72 @@ using System;
 public class CsvReader : MonoBehaviour
 {
 
-    string timeSeriesFile = @"Assets/CSVFiles/20230202_183359participantNumber6session1conditionA.log.csv";
-    //StreamReader reader;
-    // Start is called before the first frame update
+    string timeSeriesFileP1 = @"Assets/CSVFiles/20230202_183359participantNumber6session1conditionA.log.csv";
+    string timeSeriesFileP2 = @"Assets/CSVFiles/20230202_183359participantNumber6session1conditionA.log.csv";
 
-    float[] rightHandX = new float[29056];
-    float[] rightHandY = new float[29056];
-    float[] rightHandZ = new float[29056];
+    //P1
+    float[] P1rightHandX = new float[29056];
+    float[] P1rightHandY = new float[29056];
+    float[] P1rightHandZ = new float[29056];
 
-    float[] leftHandX = new float[29056];
-    float[] leftHandY = new float[29056];
-    float[] leftHandZ = new float[29056];
+    float[] P1leftHandX = new float[29056];
+    float[] P1leftHandY = new float[29056];
+    float[] P1leftHandZ = new float[29056];
 
-    float[] headX = new float[29056];
-    float[] headY = new float[29056];
-    float[] headZ = new float[29056];
+    float[] P1headX = new float[29056];
+    float[] P1headY = new float[29056];
+    float[] P1headZ = new float[29056];
 
 
-    float[] ROT_rightHandX = new float[29056];
-    float[] ROT_rightHandY = new float[29056];
-    float[] ROT_rightHandZ = new float[29056];
+    float[] P1ROT_rightHandX = new float[29056];
+    float[] P1ROT_rightHandY = new float[29056];
+    float[] P1ROT_rightHandZ = new float[29056];
 
-    float[] ROT_leftHandX = new float[29056];
-    float[] ROT_leftHandY = new float[29056];
-    float[] ROT_leftHandZ = new float[29056];
+    float[] P1ROT_leftHandX = new float[29056];
+    float[] P1ROT_leftHandY = new float[29056];
+    float[] P1ROT_leftHandZ = new float[29056];
 
-    float[] ROT_headX = new float[29056];
-    float[] ROT_headY = new float[29056];
-    float[] ROT_headZ = new float[29056];
+    float[] P1ROT_headX = new float[29056];
+    float[] P1ROT_headY = new float[29056];
+    float[] P1ROT_headZ = new float[29056];
+
+
+    //P2
+    float[] P2rightHandX = new float[29056];
+    float[] P2rightHandY = new float[29056];
+    float[] P2rightHandZ = new float[29056];
+
+    float[] P2leftHandX = new float[29056];
+    float[] P2leftHandY = new float[29056];
+    float[] P2leftHandZ = new float[29056];
+
+    float[] P2headX = new float[29056];
+    float[] P2headY = new float[29056];
+    float[] P2headZ = new float[29056];
+
+
+    float[] P2ROT_rightHandX = new float[29056];
+    float[] P2ROT_rightHandY = new float[29056];
+    float[] P2ROT_rightHandZ = new float[29056];
+
+    float[] P2ROT_leftHandX = new float[29056];
+    float[] P2ROT_leftHandY = new float[29056];
+    float[] P2ROT_leftHandZ = new float[29056];
+
+    float[] P2ROT_headX = new float[29056];
+    float[] P2ROT_headY = new float[29056];
+    float[] P2ROT_headZ = new float[29056];
 
 
     int lineNumber = 0;
     int idx = 0;
+
+    GameObject P1HeadGO, P1LeftHandGO, P1RightHandGO;
+    public GameObject cubePf;
     
     void Start()
     {
-        using (StreamReader reader = new StreamReader(timeSeriesFile))
+        using (StreamReader reader = new StreamReader(timeSeriesFileP1))
         {
             string line;
             while ((line = reader.ReadLine()) != null)
@@ -57,29 +87,29 @@ public class CsvReader : MonoBehaviour
                         for (int i = 0; i < parts.Length; i++)
                         {
                             //Debug.Log(parts[i]);
-                            headX[idx] = Single.Parse(parts[2]);
-                            headY[idx] = Single.Parse(parts[3]);
-                            headZ[idx] = Single.Parse(parts[4]);
+                            P1headX[idx] = Single.Parse(parts[2]);
+                            P1headY[idx] = Single.Parse(parts[3]);
+                            P1headZ[idx] = Single.Parse(parts[4]);
 
-                            ROT_headX[idx] = Single.Parse(parts[5]);
-                            ROT_headY[idx] = Single.Parse(parts[6]);
-                            ROT_headZ[idx] = Single.Parse(parts[7]);
+                            P1ROT_headX[idx] = Single.Parse(parts[5]);
+                            P1ROT_headY[idx] = Single.Parse(parts[6]);
+                            P1ROT_headZ[idx] = Single.Parse(parts[7]);
 
-                            leftHandX[idx] = Single.Parse(parts[8]);
-                            leftHandY[idx] = Single.Parse(parts[9]);
-                            leftHandZ[idx] = Single.Parse(parts[10]);
+                            P1leftHandX[idx] = Single.Parse(parts[8]);
+                            P1leftHandY[idx] = Single.Parse(parts[9]);
+                            P1leftHandZ[idx] = Single.Parse(parts[10]);
 
-                            ROT_leftHandX[idx] = Single.Parse(parts[11]);
-                            ROT_leftHandY[idx] = Single.Parse(parts[12]);
-                            ROT_leftHandZ[idx] = Single.Parse(parts[13]);
+                            P1ROT_leftHandX[idx] = Single.Parse(parts[11]);
+                            P1ROT_leftHandY[idx] = Single.Parse(parts[12]);
+                            P1ROT_leftHandZ[idx] = Single.Parse(parts[13]);
 
-                            rightHandX[idx] = Single.Parse(parts[20]);
-                            rightHandY[idx] = Single.Parse(parts[21]);
-                            rightHandZ[idx] = Single.Parse(parts[22]);
+                            P1rightHandX[idx] = Single.Parse(parts[20]);
+                            P1rightHandY[idx] = Single.Parse(parts[21]);
+                            P1rightHandZ[idx] = Single.Parse(parts[22]);
 
-                            ROT_rightHandX[idx] = Single.Parse(parts[23]);
-                            ROT_rightHandY[idx] = Single.Parse(parts[24]);
-                            ROT_rightHandZ[idx] = Single.Parse(parts[25]);
+                            P1ROT_rightHandX[idx] = Single.Parse(parts[23]);
+                            P1ROT_rightHandY[idx] = Single.Parse(parts[24]);
+                            P1ROT_rightHandZ[idx] = Single.Parse(parts[25]);
                         }
                     }
 
@@ -90,20 +120,59 @@ public class CsvReader : MonoBehaviour
              
             }
         }
-        Debug.Log(lineNumber);
 
-        for (int i = 0; i < headX.Length; i++) 
+        /*Debug.Log(lineNumber);
+
+        for (int i = 0; i < P1headX.Length; i++) 
         {
-            Debug.Log("rhzrot=" + ROT_rightHandZ[i]);
-        }
-
-
+            Debug.Log("rhzrot=" + P1ROT_rightHandZ[i]);
+        }*/
+        idx = 0;
+        SpawnHeadAndHands();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(idx< 29056)
+        {
+            UpdateHeadAndHands(idx);
+            idx++;
+        }
         
     }
+
+    void SpawnHeadAndHands()
+    {
+        Debug.Log("spawning head n hands");
+        P1HeadGO = Instantiate(cubePf);
+        P1HeadGO.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+        P1HeadGO.transform.SetParent(transform, false);
+
+
+        P1LeftHandGO = Instantiate(cubePf);
+        P1LeftHandGO.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        P1LeftHandGO.transform.SetParent(transform, false);
+
+
+        P1RightHandGO = Instantiate(cubePf);
+        P1RightHandGO.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        P1RightHandGO.transform.SetParent(transform, false);
+
+    }
+
+    void UpdateHeadAndHands(int idx)
+    {
+        P1HeadGO.transform.position = new Vector3(P1headX[idx], P1headY[idx], P1headZ[idx]);
+        P1HeadGO.transform.eulerAngles = new Vector3(P1ROT_headX[idx], P1ROT_headY[idx], P1ROT_headZ[idx]);
+
+        P1LeftHandGO.transform.position = new Vector3(P1leftHandX[idx], P1leftHandY[idx], P1leftHandZ[idx]);
+        P1LeftHandGO.transform.eulerAngles = new Vector3(P1ROT_leftHandX[idx], P1ROT_leftHandY[idx], P1ROT_leftHandZ[idx]);
+
+        P1RightHandGO.transform.position = new Vector3(P1rightHandX[idx], P1rightHandY[idx], P1rightHandZ[idx]);
+        P1RightHandGO.transform.eulerAngles = new Vector3(P1ROT_rightHandX[idx], P1ROT_rightHandY[idx], P1ROT_rightHandZ[idx]);
+
+    }
+
 }
